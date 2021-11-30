778ca50bb881726bc4cac9fd10fe04181d71ce88
Megan <smith.megan.design@gmail.com>
2021-11-30 05:53:28 -0700

    additional console.log removed

--- src/sagas.js
+++ src/sagas.js
@@ -5,7 +5,6 @@ import{fetchData} from'./api';
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
 function* getApiData(action) {
    try {
-     console.log(action, "test")
      const data = yield call(fetchData,action.params);
      yield put(receiveApiData(data));
     } catch (e) {
